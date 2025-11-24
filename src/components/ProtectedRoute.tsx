8570a1e540dcbb17e96c609819d237b27d1f0c0a
shadeshsaha <shadesh15-8599@diu.edu.bd>
2025-11-24 13:52:20 +0600

    upate: Updated Protected Route Component

---
+++ src/components/ProtectedRoute.tsx
@@ -0,0 +1,22 @@
+import type { ReactNode } from "react";
+import { useSelector } from "react-redux";
+import { Navigate } from "react-router-dom";
+import type { RootState } from "../redux/store";
+
+interface Props {
+  children: ReactNode;
+}
+
+const ProtectedRoute = ({ children }: Props) => {
+  const isAuthenticated = useSelector(
+    (state: RootState) => state.auth.isAuthenticated
+  );
+
+  if (!isAuthenticated) {
+    return <Navigate to="/login" replace />;
+  }
+
+  return <>{children}</>;
+};
+
+export default ProtectedRoute;
--- src/components/toast/ToastContainer.tsx
+++
@@ -1,6 +0,0 @@
-import { ToastContainer as ReactToastifyContainer } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
-
-export default function ToastContainer() {
-  return <ReactToastifyContainer position="top-right" autoClose={3000} />;
-}
