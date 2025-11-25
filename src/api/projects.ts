8f124732af35c213931fe9dca63f1b2725c6c09b
shadeshsaha <shadesh15-8599@diu.edu.bd>
2025-11-25 14:25:58 +0600

    update: API for Projects

--- src/api/projects.ts
+++ src/api/projects.ts
@@ -1,9 +1,32 @@
-// /* eslint-disable @typescript-eslint/no-explicit-any */
-// import api from "./axios";
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import axios from "axios";
 
-// export const fetchProjects = () => api.get("/projects");
-// export const fetchProject = (id: number) => api.get(`/projects/${id}`);
-// export const createProject = (payload: any) => api.post("/projects", payload);
-// export const updateProject = (id: number, payload: any) =>
-//   api.put(`/projects/${id}`, payload);
-// export const deleteProject = (id: number) => api.delete(`/projects/${id}`);
+const API_URL = "http://localhost:5000/api";
+
+export const getProjects = async (token: string) => {
+  const res = await axios.get(`${API_URL}/projects`, {
+    headers: { Authorization: `Bearer ${token}` },
+  });
+  return res.data.projects;
+};
+
+export const createProject = async (data: any, token: string) => {
+  const res = await axios.post(`${API_URL}/projects`, data, {
+    headers: { Authorization: `Bearer ${token}` },
+  });
+  return res.data.project;
+};
+
+export const updateProject = async (id: number, data: any, token: string) => {
+  const res = await axios.put(`${API_URL}/projects/${id}`, data, {
+    headers: { Authorization: `Bearer ${token}` },
+  });
+  return res.data.project;
+};
+
+export const deleteProject = async (id: number, token: string) => {
+  const res = await axios.delete(`${API_URL}/projects/${id}`, {
+    headers: { Authorization: `Bearer ${token}` },
+  });
+  return res.data;
+};
