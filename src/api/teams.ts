382989f3cb7d92bccd788d372130e4b53a9b14e8
shadeshsaha <shadesh15-8599@diu.edu.bd>
2025-11-25 14:41:06 +0600

    update: API for Teams

--- src/api/teams.ts
+++ src/api/teams.ts
@@ -1,19 +1,64 @@
-// /* eslint-disable @typescript-eslint/no-explicit-any */
-// import api from "./axios";
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import axios from "axios";
 
-// export const fetchTeams = () => api.get("/teams"); // expects members included
-// export const fetchTeam = (id: number) => api.get(`/teams/${id}`);
-// export const createTeam = (payload: any) => api.post("/teams", payload);
-// export const updateTeam = (id: number, payload: any) =>
-//   api.put(`/teams/${id}`, payload);
-// export const deleteTeam = (id: number) => api.delete(`/teams/${id}`);
+const API_URL = "http://localhost:5000/api";
 
-// export const addTeamMember = (teamId: number, payload: any) =>
-//   api.post(`/teams/${teamId}/members`, payload);
-// export const updateTeamMember = (
-//   teamId: number,
-//   memberId: number,
-//   payload: any
-// ) => api.put(`/teams/${teamId}/members/${memberId}`, payload);
-// export const deleteTeamMember = (teamId: number, memberId: number) =>
-//   api.delete(`/teams/${teamId}/members/${memberId}`);
+export const getTeams = async (token: string) => {
+  const res = await axios.get(`${API_URL}/teams`, {
+    headers: { Authorization: `Bearer ${token}` },
+  });
+  return res.data.teams;
+};
+
+export const createTeam = async (data: any, token: string) => {
+  const res = await axios.post(`${API_URL}/teams`, data, {
+    headers: { Authorization: `Bearer ${token}` },
+  });
+  return res.data.team;
+};
+
+export const updateTeam = async (id: number, data: any, token: string) => {
+  const res = await axios.put(`${API_URL}/teams/${id}`, data, {
+    headers: { Authorization: `Bearer ${token}` },
+  });
+  return res.data.team;
+};
+
+export const deleteTeam = async (id: number, token: string) => {
+  const res = await axios.delete(`${API_URL}/teams/${id}`, {
+    headers: { Authorization: `Bearer ${token}` },
+  });
+  return res.data;
+};
+
+export const getMembers = async (teamId: number, token: string) => {
+  const res = await axios.get(`${API_URL}/teams/${teamId}/members`, {
+    headers: { Authorization: `Bearer ${token}` },
+  });
+  return res.data.members;
+};
+
+export const createMember = async (
+  teamId: number,
+  data: any,
+  token: string
+) => {
+  const res = await axios.post(`${API_URL}/teams/${teamId}/members`, data, {
+    headers: { Authorization: `Bearer ${token}` },
+  });
+  return res.data.member;
+};
+
+export const updateMember = async (id: number, data: any, token: string) => {
+  const res = await axios.put(`${API_URL}/members/${id}`, data, {
+    headers: { Authorization: `Bearer ${token}` },
+  });
+  return res.data.member;
+};
+
+export const deleteMember = async (id: number, token: string) => {
+  const res = await axios.delete(`${API_URL}/members/${id}`, {
+    headers: { Authorization: `Bearer ${token}` },
+  });
+  return res.data;
+};
